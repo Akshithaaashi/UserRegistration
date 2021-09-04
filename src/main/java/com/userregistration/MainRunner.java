b7d2f7f79f4e7e9d1499d2027a404fd553a24907
Akshithaaashi <akshitha.aashi@gmail.com>
2021-09-04 08:11:14 +0530

    Added : refactor code

---
+++ src/main/java/com/userregistration/MainRunner.java
@@ -0,0 +1,24 @@
+package com.userregistration;
+
+/**
+* This is Main function
+* Calling methods to get user input for registration and validate
+*/
+public class MainRunner {
+
+	public static void main(String[] args) {
+		//Welcome Message
+		System.out.println("Program for the User Registration and Validation");
+		
+		UserRegistration userRegistration=new UserRegistration();//Object
+		
+		//Calling Methods
+		userRegistration.userFirstName();
+		userRegistration.userLastName();
+		userRegistration.userPhoneNumber();
+		userRegistration.userEmail();
+		userRegistration.userPassword();
+		
+	}
+
+}
--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -3,81 +3,55 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-    public class UserRegistration {
-        // Scanner class for user input
-        Scanner sc = new Scanner(System.in);
+public class UserRegistration {
+	
+	// Objects
+	static Scanner scanner = new Scanner(System.in);
+	static Validater validate = new Validater();
 
-        // method to check username Valid or Invalid
-        public boolean validateUsername(String userName) {
-            // regex pattern for username
-            String regex = "^[A-Z]{1}[a-zA-z0-9]{2,}$";
-            Pattern p = Pattern.compile(regex);
-            Matcher matcher = p.matcher(userName);
-            boolean result = matcher.matches();
-            if(result)
-                return true;
-            else
-                return false;
-        }
+	/** Ability to ask user for his first name and Checks it */
+	public void userFirstName() {
+		System.out.println("\nEnter the First Name:");
+		boolean result = validate.checkName(scanner.nextLine());
+		output(result);
+		if (result == false) userFirstName();
+	}
 
-        // method to check LastName Valid or Invalid
-        public boolean validateLastname(String lastName) {
-            // regex pattern for Lastname
-            String regex = "^[A-Z]{1}[a-zA-z0-9]{2,}$";
-            Pattern p = Pattern.compile(regex);
-            Matcher matcher = p.matcher(lastName);
-            boolean result = matcher.matches();
-            if(result)
-                return true;
-            else
-                return false;
-        }
+	/** Ability to ask user for his last name and Checks it */
+	public void userLastName() {
+		System.out.println("\nEnter the Last Name:");
+		boolean result = validate.checkName(scanner.nextLine());
+		output(result);
+		if (result == false) userLastName();
+	}
 
-        // method to check Email Valid or Invalid
-        public boolean validateEmail(String email) {
-            /*
-             * regex pattern for email 1)must contain character before @ 2)must contain @
-             * symbol after char 3)must contain char after @ 4)must contain "." symbol
-             * before com or in
-             */
-            String regex = "^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?";
-            Pattern p = Pattern.compile(regex);
-            Matcher matcher = p.matcher(email);
-            boolean result = matcher.matches();
-
-            if(result)
-                return true;
-            else
-                return false;
-        }
-
-        // method to check Phoneno Valid or Invalid
-        public boolean validateMobileNo(String mobileno) {
-            // regex pattern for email
-            String regex = "^[1-9]{2}[0-9]{10}$";
-            Pattern p = Pattern.compile(regex);
-            Matcher matcher = p.matcher(mobileno);
-            boolean result = matcher.matches();
-            if(result)
-                return true;
-            else
-                return false;
-        }
-
-        // method to check password Valid or Invalid
-        public boolean validatePassword(String password) {
-            /*
-             * regex pattern for password: 1)must contain atleast 8 characters 2)must
-             * contain one UpperCase 3)should have 1 numericno 4)has contain Exactily one
-             * specialSymbol.
-             */
-            String regex = "^[A-Z]{1}+[a-zA-z1-9]{5,}[@$^]{1}[1-9]{1}$";
-            Pattern p = Pattern.compile(regex);
-            Matcher matcher = p.matcher(password);
-            boolean result = matcher.matches();
-            if(result)
-                return true;
-            else
-                return false;
-        }
-    }
+	/** Ability to ask user for his E-mail and checks it */
+	public void userEmail() {
+		System.out.println("\nEnter the E-mail :");
+		boolean result = validate.checkEmail(scanner.nextLine());
+		output(result);
+		if (result == false) userEmail();
+	}
+	
+	/** Ability to ask user for his Phone Number and checks it */
+	public void userPhoneNumber() {
+		System.out.println("\nEnter the Phone Number :");
+		boolean result = validate.checkMobileNumber(scanner.nextLine());
+		output(result);
+		if (result == false) userPhoneNumber();
+	}
+	
+	/** Ability to ask user for new Password and check it */
+	public void userPassword() {
+		System.out.println("\nEnter the Password :");
+		boolean result = validate.checkPassword(scanner.nextLine());
+		output(result);
+		if (result == false) userPassword();
+	}
+	
+	/** Ability to print valid or not based on result */
+	public void output(boolean result) {
+		if (result) System.out.println("Valid Input");
+		else System.out.println("Invalid Input");
+	}
+}
